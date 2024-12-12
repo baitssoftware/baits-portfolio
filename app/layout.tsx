fe0e5d0c63601405b319262b52005d28538bc200
Sabbir Mohammad Sami <smd71430@gmail.com>
2024-12-12 11:26:34 +0600

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,91 +2,51 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-// Configure DM Sans font
-const dmSans = localFont({
+// Configure Anek Bangla font
+const anekBangla = localFont({
   src: [
     {
-      path: "./fonts/DMSans-Thin.ttf",
+      path: "./fonts/Anek-Bangla-Thin.ttf",
       weight: "100",
       style: "normal",
     },
     {
-      path: "./fonts/DMSans-ThinItalic.ttf",
-      weight: "100",
-      style: "italic",
-    },
-    {
-      path: "./fonts/DMSans-Light.ttf",
+      path: "./fonts/Anek-Bangla-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/DMSans-LightItalic.ttf",
-      weight: "300",
-      style: "italic",
+      path: "./fonts/Anek-Bangla-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
     },
     {
-      path: "./fonts/DMSans-Regular.ttf",
+      path: "./fonts/Anek-Bangla-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/DMSans-Italic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "./fonts/DMSans-Medium.ttf",
+      path: "./fonts/Anek-Bangla-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/DMSans-MediumItalic.ttf",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "./fonts/DMSans-SemiBold.ttf",
+      path: "./fonts/Anek-Bangla-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/DMSans-SemiBoldItalic.ttf",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "./fonts/DMSans-Bold.ttf",
+      path: "./fonts/Anek-Bangla-Bold.ttf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/DMSans-BoldItalic.ttf",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "./fonts/DMSans-ExtraBold.ttf",
+      path: "./fonts/Anek-Bangla-ExtraBold.ttf",
       weight: "800",
       style: "normal",
-    },
-    {
-      path: "./fonts/DMSans-ExtraBoldItalic.ttf",
-      weight: "800",
-      style: "italic",
-    },
-    {
-      path: "./fonts/DMSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "./fonts/DMSans-BlackItalic.ttf",
-      weight: "900",
-      style: "italic",
     },
   ],
-  variable: "--font-dm-sans",
+  variable: "--font-anek-bangla",
 });
 
 export const metadata: Metadata = {
@@ -101,7 +61,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dmSans.variable} font-dm-sans`}>{children}</body>
+      <body className={anekBangla.variable}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,7 +57,7 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        helvetica: ["var(--font-dm-sans)"],
+        helvetica: ["var(--font-anek-bangla)"],
       },
     },
   },
