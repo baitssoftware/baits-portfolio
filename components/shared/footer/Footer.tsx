1fd83d4cb56789c370672313644ef7d2a149eff2
Sabbir Mohammad Sami <smd71430@gmail.com>
2024-12-14 16:49:59 +0600

    add sticky footer

--- components/shared/footer/Footer.tsx
+++ components/shared/footer/Footer.tsx
@@ -2,60 +2,70 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="px-16 min-h-screen grid grid-cols-2 pt-20 pb-14 gap-10">
-      <div className="col-span-1 flex flex-col justify-between">
-        <h2 className="text-[10rem] leading-[8rem] tracking-tighter uppercase font-bold">
-          eye- opening
-        </h2>
-        <h1 className="text-2xl tracking-tight">
-          Bangladesh Associate of IT Solution
-        </h1>
-      </div>
-      <div className="col-span-1 ">
-        <h2 className="text-[10rem] leading-[8rem] tracking-tighter uppercase font-bold">
-          awarding.
-        </h2>
-        <div className="grid grid-cols-3 gap-6 text-2xl gap-y-10 items-start pt-10">
-          <div className="col-span-3 flex flex-col justify-end">
-            <p className=" pb-4">S:</p>
-            <p className=" flex flex-col gap-0.5">
-              <Link href={"/"}>Instagram</Link>
-              <Link href={"/"}>Behance</Link>
-              <Link href={"/"}>Facebook</Link>
-              <Link href={"/"}>Linkedin</Link>{" "}
-            </p>
-          </div>
-          <div className="col-span-1 flex flex-col justify-end">
-            <p className=" pb-4">L:</p>
-            <p className=" flex flex-col gap-6">
-              <p className="">202-1965 W 4th Ave Vancouver, Canada</p>
+    <div
+      className="relative h-[105vh]"
+      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+    >
+      <div className="relative h-[calc(100vh+105vh)] -top-[100vh]">
+        <div className="h-[105vh] sticky top-[calc(100vh-105vh)]">
+          <footer className="px-16  grid grid-cols-2 pt-20 pb-14 gap-10">
+            <div className="col-span-1 flex flex-col justify-between">
+              <h2 className="text-[10rem] leading-[8rem] tracking-tighter uppercase font-bold">
+                eye- opening
+              </h2>
+              <h1 className="text-2xl tracking-tight">
+                Bangladesh Associate of IT Solution
+              </h1>
+            </div>
+            <div className="col-span-1 ">
+              <h2 className="text-[10rem] leading-[8rem] tracking-tighter uppercase font-bold">
+                awarding.
+              </h2>
+              <div className="grid grid-cols-3 gap-6 text-2xl gap-y-10 items-start pt-10">
+                <div className="col-span-3 flex flex-col justify-end">
+                  <p className=" pb-4">S:</p>
+                  <p className=" flex flex-col gap-0.5">
+                    <Link href={"/"}>Instagram</Link>
+                    <Link href={"/"}>Behance</Link>
+                    <Link href={"/"}>Facebook</Link>
+                    <Link href={"/"}>Linkedin</Link>{" "}
+                  </p>
+                </div>
+                <div className="col-span-1 flex flex-col justify-end">
+                  <p className=" pb-4">L:</p>
+                  <p className=" flex flex-col gap-6">
+                    <p className="">202-1965 W 4th Ave Vancouver, Canada</p>
 
-              <div className="">30 Chukarina St Lviv, Bangladesh</div>
-            </p>
-          </div>
-          <div className="col-span-1"></div>
-          <div className="col-span-1 flex flex-col justify-end">
-            <p className=" pb-4">M:</p>
-            <p className=" flex flex-col  gap-0.5">
-              <Link href={"/"}>Home</Link>
-              <Link href={"/"}>About Us</Link>
-              <Link href={"/"}>Contact</Link>
-              <Link href={"/"}>Services</Link>
-              <Link href={"/"}>Blogs</Link> <Link href={"/"}>Our Works</Link>{" "}
-            </p>
-          </div>
-          <div className="col-span-3 flex flex-col justify-end">
-            <p className=" pb-4">E:</p>
-            <p className=" flex flex-col  gap-0.5">hello@baits.com</p>
-          </div>
-          <div className="col-span-3 flex flex-col justify-end">
-            <p className=" flex flex-col  gap-0.5">
-              © BAITS design 2024. Legal Terms
-            </p>
-          </div>
+                    <div className="">30 Chukarina St Lviv, Bangladesh</div>
+                  </p>
+                </div>
+                <div className="col-span-1"></div>
+                <div className="col-span-1 flex flex-col justify-end">
+                  <p className=" pb-4">M:</p>
+                  <p className=" flex flex-col  gap-0.5">
+                    <Link href={"/"}>Home</Link>
+                    <Link href={"/"}>About Us</Link>
+                    <Link href={"/"}>Contact</Link>
+                    <Link href={"/"}>Services</Link>
+                    <Link href={"/"}>Blogs</Link>{" "}
+                    <Link href={"/"}>Our Works</Link>{" "}
+                  </p>
+                </div>
+                <div className="col-span-3 flex flex-col justify-end">
+                  <p className=" pb-4">E:</p>
+                  <p className=" flex flex-col  gap-0.5">hello@baits.com</p>
+                </div>
+                <div className="col-span-3 flex flex-col justify-end">
+                  <p className=" flex flex-col  gap-0.5">
+                    © BAITS design 2024. Legal Terms
+                  </p>
+                </div>
+              </div>
+            </div>
+          </footer>
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 
